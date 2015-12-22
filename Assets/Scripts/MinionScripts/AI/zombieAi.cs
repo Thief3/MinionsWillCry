e1528e9f8193a0f89654ec043ddd031992b9f0c0
Malik Kissarli <kissarlim@gmail.com>
2015-12-22 02:05:04 +0000

    Pretty Certain Ai is golden for the zombie minion, and it's just the collisions

--- Assets/Scripts/MinionScripts/AI/zombieAi.cs
+++ Assets/Scripts/MinionScripts/AI/zombieAi.cs
@@ -3,80 +3,44 @@ using System.Collections;
 using Pathfinding;
 
 public class zombieAi : MonoBehaviour {
-    //The point to move to
-    public Vector3 targetPosition;
+    public Vector3 target;
 
     private Seeker seeker;
-    private Rigidbody2D controller;
 
-    //The calculated path
     public Path path;
 
-    //The AI's speed per second
-    public float speed = 100;
+    public float speed, distanceToNextWaypoint;
 
-    //The max distance from the AI to a waypoint for it to continue to the next waypoint
-    public float nextWaypointDistance = 3;
+    private int currentWaypoint;
+    private Rigidbody2D rb;
 
-    //The waypoint we are currently moving towards
-    private int currentWaypoint = 0;
-
-    public float repathRate = 0.5f;
-    private float lastRepath = -9999;
-
-    public void Start() {
-        targetPosition = new Vector3(0, 0, 0);
+    void Start() {
+        currentWaypoint = 0;
         seeker = GetComponent<Seeker>();
-        controller = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
 
-        //Start a new path to the targetPosition, return the result to the OnPathComplete function
-        //seeker.StartPath (transform.position,targetPosition, OnPathComplete);
+        seeker.StartPath(transform.position, target, OnPathComplete);
     }
 
     public void OnPathComplete(Path p) {
-        p.Claim(this);
-        if (!p.error) {
-            if (path != null) path.Release(this);
-            path = p;
-            //Reset the waypoint counter
-            currentWaypoint = 0;
-        }
-        else {
-            p.Release(this);
-            Debug.Log("Oh noes, the target was not reachable: " + p.errorLog);
-        }
-
-        //seeker.StartPath (transform.position,targetPosition, OnPathComplete);
+        path = p;
     }
 
-    public void Update() {
-        if (Time.time - lastRepath > repathRate && seeker.IsDone()) {
-            lastRepath = Time.time + Random.value * repathRate * 0.5f;
-            seeker.StartPath(transform.position, targetPosition, OnPathComplete);
-        }
-
+    void Update () {
         if (path == null) {
-            //We have no path to move after yet
-            return;
+            Debug.Log("Yeah it didn't work....");
+        }
+        else if (currentWaypoint >= path.vectorPath.Count) {
+            Debug.Log("Done...?");
         }
 
-        if (currentWaypoint > path.vectorPath.Count) return;
-        if (currentWaypoint == path.vectorPath.Count) {
-            Debug.Log("End Of Path Reached");
-            currentWaypoint++;
-            return;
-        }
-
-        //Direction to the next waypoint
         Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
-        dir *= speed;// * Time.deltaTime;
-        //transform.Translate (dir);
-        controller.velocity = dir;
+        rb.velocity = speed * dir;
 
-        //if (Vector3.Distance (transform.position,path.vectorPath[currentWaypoint]) < nextWaypointDistance) {
-        if ((transform.position - path.vectorPath[currentWaypoint]).sqrMagnitude < nextWaypointDistance * nextWaypointDistance) {
+        if (Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]) < distanceToNextWaypoint) {
             currentWaypoint++;
-            return;
         }
     }
+
+
 }

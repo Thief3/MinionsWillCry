b1540fbc349c751e67015f20c6b51bb8a55353fc
Malik Kissarli <kissarlim@gmail.com>
2015-12-22 02:55:40 +0000

    Walls now have a back and front

---
+++ Assets/Scripts/layerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class layerController : MonoBehaviour {
+    public float gridSize;
+    private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float layer = (transform.position.y - sr.bounds.size.y/2) / gridSize;
+        sr.sortingOrder = -Mathf.RoundToInt(layer);
+    }
+}
